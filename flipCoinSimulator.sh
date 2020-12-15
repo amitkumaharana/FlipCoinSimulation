639c159761c586df7bb243a19b9f18d5f3148e47
amitkumaharana <ak657maharana@gmail.com>
2020-12-15 16:27:26 +0530

    Looping and counting the number of times head or tail comes

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash
 echo Welcome to the Flip Coin Simulator program
+count_head=0
+count_tail=0
+for((i=0;i<20;i++))
+do
 flip=$((RANDOM%2))
 if [ $flip -eq 0 ]
 then
-	echo HEADS
+count_head=$((count_head+1))
 else
-	echo TAILS
+count_tail=$((count_tail+1))
 fi
-
+done
+echo "HEAD shows for $count_head times"
+echo "TAIL shows for $count_tail times"
