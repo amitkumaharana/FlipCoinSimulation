4d5fd2d238e4e187f4061dcb9da7c95e7adebd97
amitkumaharana <ak657maharana@gmail.com>
2020-12-15 18:11:11 +0530

    Condition 21 wins and the difference of win or tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,7 +2,7 @@
 echo Welcome to the Flip Coin Simulator program
 count_head=0
 count_tail=0
-for((i=0;i<20;i++))
+while [ $count_head -lt 21 -a $count_tail -lt 21 ]
 do
 flip=$((RANDOM%2))
 if [ $flip -eq 0 ]
@@ -12,5 +12,16 @@ else
 count_tail=$((count_tail+1))
 fi
 done
-echo "HEAD shows for $count_head times"
-echo "TAIL shows for $count_tail times"
+echo HEAD gets $count_head
+echo TAIL gets $count_tail
+if [ $count_head -gt $count_tail ]
+then
+	diff=$((count_head-count_tail))
+	echo HEAD wins by $diff
+elif [ $count_tail -gt $count_head ]
+then
+	diff=$((count_tail-count_head))
+	echo TAIL wins by $diff
+else
+	echo It is a tie
+fi
