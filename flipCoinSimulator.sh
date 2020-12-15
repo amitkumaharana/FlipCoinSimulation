a93eb0ce9ade33fa6f10712a145c5f7732383176
amitkumaharana <ak657maharana@gmail.com>
2020-12-15 18:43:18 +0530

    Ensuring that if tied then process to go on till difference is atleast 2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -12,8 +12,6 @@ else
 count_tail=$((count_tail+1))
 fi
 done
-echo HEAD gets $count_head
-echo TAIL gets $count_tail
 if [ $count_head -gt $count_tail ]
 then
 	diff=$((count_head-count_tail))
@@ -23,5 +21,24 @@ then
 	diff=$((count_tail-count_head))
 	echo TAIL wins by $diff
 else
-	echo It is a tie
+	while [ $diff -lt 2 ]
+	do
+		flip=$((RANDOM%2))
+		if [ $flip -eq 0 ]
+		then
+			count_head=$((count_head+1))
+		else
+			count_tail=$((count_tail+1))
+		fi
+		if [ $count_head -gt $count_tail ]
+		then
+        		diff=$((count_head-count_tail))
+		elif [ $count_tail -gt $count_head ]
+		then
+        		diff=$((count_tail-count_head))
+		fi
+	done
+
 fi
+echo HEAD counts to $count_head
+echo TAIL counts to $count_tail
